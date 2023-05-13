fb548d1ec2aa7d8546d6837bde6339661e758dc6
dev.pettersbrazil <dev.petters.brazil@gmail.com>
2023-05-13 15:48:29 -0300

    feat: hash router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { HashLocationStrategy, LocationStrategy } from "@angular/common";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { HttpClientModule } from "@angular/common/http";
@@ -25,7 +26,11 @@ import { AppComponent } from "./app.component";
   ],
   providers: [
     AuthInterceptorProvider,
-    ErrorInterceptorProvider
+    ErrorInterceptorProvider,
+    {
+      provide: LocationStrategy,
+      useClass: HashLocationStrategy
+    }
   ],
   bootstrap: [AppComponent],
 })
